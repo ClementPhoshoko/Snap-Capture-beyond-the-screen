dcb7b379c6583d76437b8eeac7661953038a66b8
Clement Phoshoko <Clement.Phoshoko@outlook.com>
2026-07-28 14:24:01 +0200

    update folder structure + readme

---
+++ src/popup/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>AkovoLabs Snap</h1>
+      <p>Capture beyond the screen.</p>
+    </div>
+  );
+}
+
+export default App;
